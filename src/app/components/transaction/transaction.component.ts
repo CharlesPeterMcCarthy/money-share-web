9d6aea88853ec5186006712c45ddfdc28bf8de86
Charles McCarthy <charles@studybundles.com>
2020-04-15 18:30:05 +0100

    Transaction Component

---
+++ src/app/components/transaction/transaction.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { faEuroSign, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Transaction } from '@moneyshare/common-types';
+
+@Component({
+  selector: 'transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.styl']
+})
+export class TransactionComponent implements OnInit {
+
+  @Input() public transaction: Transaction;
+  public euroIcon: IconDefinition = faEuroSign;
+  public arrowRightIcon: IconDefinition = faArrowRight;
+
+  public constructor() { }
+
+  public ngOnInit(): void { }
+
+  public transactionDirection = (): string => {
+    switch (this.transaction.type) {
+      case 'DEPOSIT':
+      case 'RECEIVE':
+        return 'positive';
+      case 'WITHDRAW':
+      case 'SEND':
+        return 'negative';
+      default:
+        return '';
+    }
+  }
+
+}
