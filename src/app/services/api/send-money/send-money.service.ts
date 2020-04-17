e42c22bf0423a9267864c501809e3d1d5de61bea
Charles McCarthy <charles@studybundles.com>
2020-04-17 13:38:00 +0100

    Transfer (Send Money) API Service

---
+++ src/app/services/api/send-money/send-money.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { CustomResponse } from '../../auth/auth.service';
+import { API } from 'aws-amplify';
+import { Transfer } from '@moneyshare/common-types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SendMoneyAPIService {
+
+  public constructor(
+    private _api: ApiService
+  ) { }
+
+  public SendMoney = (transfer: Transfer): Promise<CustomResponse> =>
+    API.put(this._api.name, `/transfer`, { body: { transfer } }).catch(this._api.handleError);
+
+}
