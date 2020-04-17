8719e2413804d8d321b724843226b7b5b8f6d7f8
Charles McCarthy <charles@studybundles.com>
2020-04-17 11:06:16 +0100

    Withdraw API Service

---
+++ src/app/services/api/withdraw/withdraw.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { CustomResponse } from '../../auth/auth.service';
+import { API } from 'aws-amplify';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WithdrawAPIService {
+
+  public constructor(
+    private _api: ApiService
+  ) { }
+
+  public WithdrawMoney = (amount: number): Promise<CustomResponse> =>
+    API.put(this._api.name, `/withdraw/${amount}`, '').catch(this._api.handleError);
+
+}
