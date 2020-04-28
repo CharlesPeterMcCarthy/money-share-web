2de017b5fa2a0619115dc6b40bd0d52a9711c073
Charles McCarthy <charles@studybundles.com>
2020-04-28 17:34:19 +0100

    User Item Image Skew Fix

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { User } from '@moneyshare/common-types';
+import { Component, Input, OnInit } from '@angular/core';
+import { MyUserProfile, User } from '@moneyshare/common-types';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { MoneyBottomSheetComponent } from '../money-bottom-sheet/money-bottom-sheet.component';
 
 @Component({
   selector: 'user-item',
   templateUrl: './user-item.component.html',
   styleUrls: ['./user-item.component.styl']
 })
-export class UserItemComponent {
+export class UserItemComponent implements OnInit {
 
   @Input() public user: Partial<User>;
   @Input() public showMemberSince: boolean = false;
+  public avatar: SafeStyle;
+
+  public constructor(
+    private _sanitization: DomSanitizer
+  ) { }
+
+  public ngOnInit(): void {
+    this.setAvatar(this.user.avatar);
+  }
+
+  private setAvatar = (avatar: string): SafeStyle => this.avatar =
+    this._sanitization.bypassSecurityTrustStyle(`url(${ avatar || './assets/images/noavatar.jpg' })`);
 
 }
